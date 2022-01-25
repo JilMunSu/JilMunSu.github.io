b099c184330d556f3b05bc93e7e290dd263f051f
JilMunSu <sh466sh@gmail.com>
2022-01-26 01:24:39 +0900

    profile 수정

--- js/common.js
+++ js/common.js
@@ -1,31 +1,28 @@
-// $(document).ready(function () {
-//   if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-    
-//     document.documentElement.classList.add("dark");
-//   }
-// });
-
 window.onload=function(){
-  document.getElementById("toggleTheme").addEventListener("click", () => {
-    console.log('1')
+  const togglebutton = document.getElementById("toggleTheme"); 
+  togglebutton.addEventListener("click", () => {
     const html = document.documentElement;
-
     if (html.classList.contains("dark")) {
-        html.classList.remove("dark");
-        localStorage.setItem("darkTheme", "false");
+      html.classList.remove("dark");
+      localStorage.setItem("darkTheme", "false");
+      console.log('lightMode');
     } else {
-        html.classList.add("dark");
-        localStorage.setItem("darkTheme", "true");
+      html.classList.add("dark");
+      localStorage.setItem("darkTheme", "true");
+      console.log('darkMode');
     }
-});
+  });
 
-const storedTheme = localStorage.getItem("darkTheme");
-
-if (storedTheme !== null) {
+  const storedTheme = localStorage.getItem("darkTheme");
+  if (storedTheme !== null) {
     if (storedTheme === "true") {
-        document.documentElement.classList.add("dark");
+      document.documentElement.classList.add("dark");
+      togglebutton.checked = true;
+      console.log('darkMode');
     }
-} else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-    document.documentElement.classList.add("dark");
-}
+  } else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+      document.documentElement.classList.add("dark");
+      togglebutton.checked = false;
+      console.log('lightMode');
+  }
 }
