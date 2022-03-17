16490cb23a7c09d7607d2c65bff82b356ae7ff4a
JilMunSu <sh466sh@gmail.com>
2022-03-17 10:25:06 +0900

    profile 수정

--- js/common.js
+++ js/common.js
@@ -52,6 +52,92 @@ window.onload=function(){
     head.css("background", "none");
     head.css("border-bottom", "none");
   }
+
+  // a태그 url 숨기기
+  $("body").on('mouseover', 'a', function (e) {
+    var $link = $(this),
+    href = $link.attr('href') || $link.data("href");
+
+    $link.off('click.chrome');
+    $link.on('click.chrome', function () {
+       window.location.href = href;
+    }).attr('data-href', href)
+    .css({ cursor: 'pointer' })
+    .removeAttr('href'); 
+  });
+
+  // 토글버튼 동작
+  $('.btnToggle').click(function() {
+    $(this).toggleClass('toggleOn');
+    if($(this).hasClass('typeB') == true && $(this).hasClass('noText') == false) {
+      if($(this).hasClass('toggleOn')) {
+        $(this).css("padding-right", "25px");
+        $(this).css("padding-left", "0");
+      } else {
+        $(this).css("padding-right", "0");
+        $(this).css("padding-left", "25px");
+      }
+    }
+  });
+
+  // 확장버튼 동작
+  $('.btnExpan').click(function() {
+    $(this).toggleClass('toggleOn');
+    if($(this).hasClass('toggleOn') == true) {
+      $(this).next().css("display", "block");
+    } else {
+      $(this).next().css("display", "none");
+    }
+  });
+
+  // 탭 메뉴
+  $('.tabs li .tabMenu').click(function(){
+    var tabCont = $(this).attr('data-tab');
+
+    $('.tabs li .tabMenu').removeClass('current');
+    $('.tabContent').removeClass('current');
+
+    $(this).addClass('current');
+    $("#" + tabCont).addClass('current');
+  });
+
+  // 햄버거 메뉴
+  if($(this).hasClass('toggleOn')) {
+    $('.hamMenu a, .hamMenu button').attr('tabindex', '0');
+  } else {
+    $('.hamMenu a, .hamMenu button').attr('tabindex', '-1');
+  }
+
+  // 체크박스 전체선택
+  $(".checkAlls").click(function() {
+    checkAll.call($(this));
+  });
+
+  $("input[name=checkTypeB]").click(function() {
+    chkAllSense.call($(this));
+  });
+
+  // input, select 탭 키 엔터
+  $('body').on('keydown', 'input, select', function(e) {
+    if (e.key === "Enter") {
+      if($(this).is(":checked")) {
+        $(this).prop("checked", false);
+        if($(this).hasClass('checkAlls')) {
+          checkAll.call($(this));
+        } else if ($(this).attr('name') === 'checkTypeB') {
+          chkAllSense.call($(this));
+        }
+      } else {
+        $(this).prop("checked", true);
+        if($(this).hasClass('checkAlls')) {
+          checkAll.call($(this));
+        } else if ($(this).attr('name') === 'checkTypeB') {
+          chkAllSense.call($(this));
+        }
+      }
+    }
+  });
+
 }
 
 // 스크롤
@@ -59,16 +145,39 @@ function goTop() {
   $('html, body').animate({scrollTop:0}, 400);
 }
 
-// 네비게이션
+// 네비게이션 헤더있는 경우
 function moveSCF(seq) {
   var offset = $("#menu" + seq).offset().top;
   var fixedVal = offset - 70;
   $('html, body').animate({scrollTop : fixedVal}, 400);
-  return true
+  return true;
 }
 
+// 네비게이션 헤더없는 경우
 function moveSC(seq) {
   var offset = $("#menu" + seq).offset().top;
   $('html, body').animate({scrollTop : offset}, 400);
-  return true
+  return true;
+}
+
+// 체크박스 전체선택
+function checkAll() {
+  if($(this).is(":checked")) {
+    $(this).parent().nextAll().children('input').prop("checked", true);
+  }
+  else { 
+    $(this).parent().nextAll().children('input').prop("checked", false);
+  } 	
+}
+
+// 체크박스 전체선택 감지
+function chkAllSense() {
+  var total = $(this).parent().parent().find('input[name=checkTypeB]').length;
+  var checked = $(this).parent().parent().find('input[name=checkTypeB]:checked').length;
+
+  if(total != checked) {
+    $(this).parent().parent().children(':first-child').find('input').prop("checked", false);
+  } else {
+    $(this).parent().parent().children(':first-child').find('input').prop("checked", true);
+  }
 }
